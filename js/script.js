144f22f550bc7d561637431ad3dbb51f2f257268
komaliK-KK <karlapudi123K@gmail.com>
2025-06-26 12:30:01 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("Portfolio Website Loaded");
+
+// Smooth scroll for internal navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
